32d7908cc5dd1235cb2463203800c69c497a8d15
sneurlax <sneurlax@gmail.com>
2025-12-06 11:36:47 -0600

    feat: c ffi to render thread

---
+++ src/ffi.h
@@ -0,0 +1,52 @@
+#ifndef FFI_H
+#define FFI_H
+
+#include "common.h"
+#include "uniform_queue.h"
+#include "renderer.h"
+
+void deleteRenderer();
+void createRenderer(VulkanPluginContext *textureStruct);
+Renderer *getRenderer();
+
+extern "C" FFI_PLUGIN_EXPORT bool rendererStatus();
+extern "C" FFI_PLUGIN_EXPORT void getTextureSize(int32_t *width, int32_t *height);
+extern "C" FFI_PLUGIN_EXPORT void startThread();
+extern "C" FFI_PLUGIN_EXPORT void stopThread();
+extern "C" FFI_PLUGIN_EXPORT const char *setShader(
+        bool isContinuous,
+        const char *vertexShader,
+        const char *fragmentShader);
+extern "C" FFI_PLUGIN_EXPORT const char *setShaderToy(
+        const char *fragmentShader);
+extern "C" FFI_PLUGIN_EXPORT const char *getVertexShader();
+extern "C" FFI_PLUGIN_EXPORT const char *getFragmentShader();
+extern "C" FFI_PLUGIN_EXPORT void addShaderToyUniforms();
+extern "C" FFI_PLUGIN_EXPORT void setMousePosition(
+        double posX,
+        double posY,
+        double posZ,
+        double posW,
+        double textureWidgetWidth,
+        double textureWidgetHeight);
+extern "C" FFI_PLUGIN_EXPORT double getFPS();
+extern "C" FFI_PLUGIN_EXPORT bool addUniform(
+        const char *name,
+        UniformType type,
+        void *val);
+extern "C" FFI_PLUGIN_EXPORT bool removeUniform(const char *name);
+extern "C" FFI_PLUGIN_EXPORT bool setUniform(
+        const char *name,
+        void *val);
+extern "C" FFI_PLUGIN_EXPORT bool addSampler2DUniform(
+        const char *name,
+        int width,
+        int height,
+        void *val);
+extern "C" FFI_PLUGIN_EXPORT bool replaceSampler2DUniform(
+        const char *name,
+        int width,
+        int height,
+        void *val);
+
+#endif // FFI_H
