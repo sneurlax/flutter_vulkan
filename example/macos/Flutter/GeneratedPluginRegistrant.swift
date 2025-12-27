ef54be377e6213c844a0cd03c6370b4f7e09c1d8
sneurlax <sneurlax@gmail.com>
2025-12-27 13:07:00 -0600

    feat: macos example runner

---
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_vulkan
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterVulkanPlugin.register(with: registry.registrar(forPlugin: "FlutterVulkanPlugin"))
+}
