ba6ac90dfcf751dfbca311dd0dadd27e91bfaa34
sneurlax <sneurlax@gmail.com>
2026-02-01 10:24:18 -0600

    build: register web platform and add dependencies

--- lib/flutter_vulkan.dart
+++ lib/flutter_vulkan.dart
@@ -1,8 +1,10 @@
 library flutter_vulkan;
 
 export 'src/flutter_vulkan_api.dart';
-export 'src/flutter_vulkan_ffi.dart';
+export 'src/flutter_vulkan_ffi.dart'
+    if (dart.library.js_interop) 'src/vulkan_renderer.dart';
 export 'src/flutter_vulkan_method_channel.dart';
 export 'src/flutter_vulkan_platform_interface.dart';
 export 'src/vulkan_controller.dart';
+export 'src/vulkan_renderer.dart';
 export 'src/vulkan_texture.dart';
