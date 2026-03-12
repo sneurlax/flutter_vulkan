b7067eda08a487e85aa4dcd484b3b2162d607ce1
sneurlax <sneurlax@gmail.com>
2026-03-11 17:40:54 -0800

    feat(windows): add Windows platform support for Rust backend

--- lib/src/vulkan_controller_native.dart
+++ lib/src/vulkan_controller_native.dart
@@ -6,9 +6,14 @@ import 'flutter_vulkan_ffi.dart';
 import 'vulkan_renderer.dart';
 
 VulkanRenderer createRenderer() {
-  final nativeLib = Platform.isAndroid
-      ? ffi.DynamicLibrary.open('libflutter_vulkan_plugin.so')
-      : ffi.DynamicLibrary.process();
+  final ffi.DynamicLibrary nativeLib;
+  if (Platform.isAndroid) {
+    nativeLib = ffi.DynamicLibrary.open('libflutter_vulkan_plugin.so');
+  } else if (Platform.isWindows) {
+    nativeLib = ffi.DynamicLibrary.open('flutter_vulkan_plugin.dll');
+  } else {
+    nativeLib = ffi.DynamicLibrary.process();
+  }
   return FlutterVulkanFfi.fromLookup(nativeLib.lookup);
 }
 
